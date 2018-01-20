98d2a1645a3545beebbe582af935d2abf792dc20
Phoomparin Mano <work@phoom.in.th>
2018-01-20 13:09:04 +0700

    :sparkles: Added Crawled URL List

--- src/main.rs
+++ src/main.rs
@@ -8,7 +8,8 @@ use reqwest::{Client, Proxy};
 use scraper::{Html, Selector};
 
 struct Crawler {
-  client: Client
+  client: Client,
+  crawled: Vec<String>
 }
 
 impl Crawler {
@@ -20,27 +21,37 @@ impl Crawler {
       .build()
       .unwrap();
 
-    Crawler { client }
+    Crawler { client, crawled: vec![] }
   }
 
-  fn crawl(&self, url: &str) {
-    println!("Crawling {}", url);
+  fn crawl(&mut self, url: &str) {
+    if self.crawled.contains(&url.to_string()) {
+      println!("I already crawled {}! Ignoring...", url);
+    } else if url.starts_with("..") {
+      println!("Relative URL found: {}. Resolving...", url);
+    } else if !url.starts_with("http") {
+      println!("Non-HTTP URL found: {}. Resolving...", url);
+    } else {
+      println!("Crawling: {}", url);
 
-    match self.client.get(url).send() {
-      Ok(ref mut res) => {
-        let body = res.text().unwrap();
+      self.crawled.push(url.to_string());
+      // println!("Crawled List: {:?}", self.crawled);
 
-        if res.status().is_success() {
-          self.parse(&body)
-        }
-      },
-      Err(err) => println!("Network Error: {}", err)
+      match self.client.get(url).send() {
+        Ok(ref mut res) => {
+          let body = res.text().unwrap();
+
+          if res.status().is_success() {
+            println!("Successfully Crawled {}. Parsing Document...", url);
+            self.parse(&body)
+          }
+        },
+        Err(err) => println!("Network Error: {}", err)
+      }
     }
   }
 
-  fn parse(&self, body: &str) {
-    println!("Parsing Document...");
-
+  fn parse(&mut self, body: &str) {
     let document = Html::parse_document(&body);
     let links = Selector::parse("a").unwrap();
 
@@ -57,7 +68,7 @@ impl Crawler {
 fn main() {
   println!("Initializing...");
 
-  let crawler = Crawler::new();
+  let mut crawler = Crawler::new();
   crawler.crawl("http://es2adizg32j3kob5.onion");
 }
 
