6b9337a62989a6a8c19ba70fe7cf6fe835f87451
Phoomparin Mano <work@phoom.in.th>
2018-01-20 12:42:48 +0700

    :recycle: Refactored into Crawler Struct

--- src/main.rs
+++ src/main.rs
@@ -2,37 +2,62 @@ extern crate select;
 extern crate reqwest;
 extern crate scraper;
 
+use std::thread;
+use std::sync::mpsc;
 use reqwest::{Client, Proxy};
 use scraper::{Html, Selector};
 
-fn get_client() -> Client {
-    let tor_proxy = Proxy::http("http://localhost:8123").unwrap();
-
-    Client::builder()
-        .proxy(tor_proxy)
-        .build()
-        .unwrap()
+struct Crawler {
+  client: Client
 }
 
-fn main() {
-    println!("Initializing...");
-    let client = get_client();
+impl Crawler {
+  fn new() -> Crawler {
+    let tor_proxy = Proxy::http("http://localhost:8123").unwrap();
 
-    println!("Sending Request...");
+    let client = Client::builder()
+      .proxy(tor_proxy)
+      .build()
+      .unwrap();
 
-    // res.status().is_success()
-    let mut res = client.get("http://es2adizg32j3kob5.onion").send().unwrap();
-    let body = res.text().unwrap();
+    Crawler { client }
+  }
 
+  fn crawl(&self, url: &str) {
+    println!("Crawling {}", url);
+
+    match self.client.get(url).send() {
+      Ok(ref mut res) => {
+        let body = res.text().unwrap();
+
+        if res.status().is_success() {
+          self.parse(&body)
+        }
+      },
+      Err(err) => println!("Network Error: {}", err)
+    }
+  }
+
+  fn parse(&self, body: &str) {
     println!("Parsing Document...");
 
     let document = Html::parse_document(&body);
     let links = Selector::parse("a").unwrap();
 
     for link in document.select(&links) {
-        let text: Vec<_> = link.text().collect();
-        let href = link.value().attr("href").unwrap();
-        println!("Link Found: {} ({:?})", href, text);
+      let text: Vec<_> = link.text().collect();
+      let url = link.value().attr("href").unwrap();
+      println!("Link Found: {} ({:?})", &url, text);
+
+      self.crawl(&url);
     }
+  }
+}
+
+fn main() {
+  println!("Initializing...");
+
+  let crawler = Crawler::new();
+  crawler.crawl("http://es2adizg32j3kob5.onion");
 }
 
